6b3b73da53d004f87affaa519360f47b18b2c7d6
Liam <liamt.hyatt@gmail.com>
2020-01-04 15:48:42 -0800

    working on webpack

--- indecision/src/playground/counter-example.js
+++ indecision/src/playground/counter-example.js
@@ -5,10 +5,23 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
-        };
+            count: 0
+        }
     }
-    handleAddOne(){
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);
+
+        if (!isNaN(count)) {
+            this.setState(() => ({ count: count}));
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count);
+        }
+    }
+    handleAddOne() {
         this.setState((prevState) => {
             return {
                 count: prevState.count + 1
@@ -42,9 +55,9 @@ class Counter extends React.Component {
     }
 }
 
-Counter.defaultProps = {
-    count: 0
-};
+// Counter.defaultProps = {
+//     count: 0
+// };
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
 
