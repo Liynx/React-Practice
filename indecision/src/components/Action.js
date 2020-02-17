077554b65fe36d626f8261df52459d3acf6aa649
Liynx <liamt.hyatt@gmail.com>
2020-02-16 22:49:40 -0800

    adding stuff after spill but with cscss working

--- indecision/src/components/Action.js
+++ indecision/src/components/Action.js
@@ -5,7 +5,9 @@ import React from 'react';
 const Action = (props) => {
     return (
         <div>
-            <button onClick={props.handlePick}
+            <button
+                className="big-button"
+                onClick={props.handlePick}
                 disabled={!props.hasOptions}
             >
                 What Should I Do?
