1166d8f72d230786c1238d201a04acd02d201c48
Liynx <liamt.hyatt@gmail.com>
2020-04-22 17:16:23 -0700

    testing commit

---
+++ boilerplate-v1/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
+
+
+ReactDOM.render(<P>This is my boilerplate</P>, document.getElementById('app'))
+
+
+
+
+
